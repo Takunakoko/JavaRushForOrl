e6af5559220e21af36a4f9c5eb01497547e7dfd8
Arkadiu <ntfs2002@gmail.com>
2016-05-04 22:11:16 +0300

    New changelist

---
+++ src/com/javarush/test/level37/lesson04/big01/male/KidBoy.java
@@ -0,0 +1,17 @@
+package com.javarush.test.level37.lesson04.big01.male;
+
+import com.javarush.test.level37.lesson04.big01.Human;
+
+/**
+ * Created by Администратор on 04.05.2016.
+ */
+public class KidBoy implements Human
+{
+    public final static int MAX_AGE = 12;
+
+    @Override
+    public String toString()
+    {
+        return "KidBoy{}";
+    }
+}
